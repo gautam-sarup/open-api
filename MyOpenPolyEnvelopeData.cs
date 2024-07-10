87ed14b52ddff821d2881b969a46f34ef6d7aaf2
Gautam Sarup <gautam_sarup@outlook.com>
2024-07-10 08:41:30 +0100

    Create Open Poly envelope service.

---
+++ MyOpenPolyEnvelopeData.cs
@@ -0,0 +1,9 @@
+namespace OpenAPI2
+{
+    public class MyOpenPolyEnvelopeData
+    {
+        public int? Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
