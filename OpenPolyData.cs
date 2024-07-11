e62e739f16c62839109f0ede8a5bdc8a23314c34
Gautam Sarup <gautam_sarup@outlook.com>
2024-07-11 13:23:00 +0100

    Remove unneed files.

--- OpenPolyData.cs
+++
@@ -1,9 +0,0 @@
-namespace OpenAPI2
-{
-    public class OpenPolyData
-    {
-        public int? Id { get; set; }
-        public string? Name { get; set; }
-        public string? Description { get; set; }
-    }
-}
